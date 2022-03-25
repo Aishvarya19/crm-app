d5be54168b5b72c389993cc1f964f4ea35f9882d
Aishvarya Muthukumar <aishvarya1904@gmail.com>
2022-03-25 17:01:22 +0530

    Completed Lead feature development
    Handing over the lead feature to QA team for testing process.

---
+++ core/lead.py
@@ -0,0 +1,20 @@
+
+
+
+#lead feature functionalities
+##
+
+#Create Lead.
+def Create_lead();
+print "lead created successfully"
+
+
+
+#Create Update.
+def Update_lead();
+print "lead updated successfully"
+
+
+#Delete Lead.
+def delete_lead();
+print "lead deleted successfully"
